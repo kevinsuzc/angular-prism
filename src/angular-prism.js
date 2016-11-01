8c078e8b345c3878bc448dd894072858408c2c6c
zichaosu <szc769104@gmail.com>
2016-11-02 04:27:43 +1100

    create

---
+++ src/angular-prism.js
@@ -0,0 +1,14 @@
+/**
+ * Created by Kevin Su on 2016/11/2.
+ */
+angular.module('angularPrism',[])
+    .directive('code', function () {
+        return {
+            restrict: 'E',
+            link: function ($scope, element, attrs) {
+                element.ready(function () {
+                    Prism.highlightAll();
+                });
+            }
+        }
+    });
